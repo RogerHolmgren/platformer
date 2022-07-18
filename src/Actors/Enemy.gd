b4bf85d632ef15afa7ab5140aaffcba7000886e1
RogerH <rogerholmgren@gmail.com>
2022-07-18 18:15:19 +0200

    GDQuest - First 2D Game part 1

---
+++ src/Actors/Enemy.gd
@@ -0,0 +1,23 @@
+extends Actor
+
+
+func _ready():
+	set_physics_process(false)
+	_velocity.x = -speed.x
+
+
+func _on_StompDetector_body_entered(body: PhysicsBody2D) -> void:
+	if body.global_position.y > get_node("StompDetector").global_position.y:
+		return
+	get_node("CollisionShape2D").disabled = true
+	queue_free()
+
+
+func _physics_process(delta):
+	_velocity.y += gravity * delta # Falling
+	
+	# Collision
+	if is_on_wall():
+		_velocity.x *= -1.0
+	
+	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
